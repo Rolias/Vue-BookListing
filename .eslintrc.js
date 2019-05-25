5177ac1b774edec913394cb5c2205318c9b25d57
Tod Gentille <tod.gentille@gmail.com>
2019-05-24 17:32:21 -0700

    add book

--- .eslintrc.js
+++ .eslintrc.js
@@ -34,6 +34,14 @@ module.exports = {
       optionalDependencies: ['test/unit/index.js']
     }],
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    quotes: ['error', 'single'],
+    semi: [
+      'error',
+      'never',
+      {
+        beforeStatementContinuationChars: 'always',
+      },
+    ],
   }
 }
--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -3,15 +3,17 @@
     <h1>{{title}}</h1>
     <ul>
       <book-item v-for="book in books" v-bind:key="book.title" :book="book"></book-item>
+      <book-form @addBook="appendBook"></book-form>
     </ul>
   </div>
 </template>
 
 <script>
 import BookItem from "./BookItem";
+import BookForm from "./BookForm";
 export default {
   name: "BookList",
-  components: { BookItem },
+  components: { BookItem, BookForm },
   data() {
     return {
       title: "All Books",
@@ -22,6 +24,11 @@ export default {
         { title: "14", author: "Peter Clines" }
       ]
     };
+  },
+  methods: {
+    appendBook(bookTitle, bookAuthor) {
+      this.books.push({ title: bookTitle, author: bookAuthor });
+    }
   }
 };
 </script>
